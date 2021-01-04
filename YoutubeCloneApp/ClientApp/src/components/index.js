c8e0afb9362ce97926352bd8e58748288d64c000
carloshernandezcoto <76927633+carloshernandezcoto@users.noreply.github.com>
2021-01-04 12:31:43 -0600

    First commit of this practice app.

---
+++ YoutubeCloneApp/ClientApp/src/components/index.js
@@ -0,0 +1,3 @@
+﻿export { default as SearchBar } from './SearchBar';
+export { default as VideoDetail } from './VideoDetail';
+export { default as VideoList } from './VideoList';
---
+++ YoutubeCloneApp/ClientApp/src/index.js
@@ -0,0 +1,18 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+
+//namespace YoutubeCloneApp.ClientApp.src
+//{
+//    public class index
+//    {
+//    }
+//}
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
